83f9f9fd7b1be97f9a72f0d0607d816621673ab2
wilo anjalo <anjalo@free.fr>
2022-04-01 15:59:52 +0200

    Ajout index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<html>
+  <body>
+      <p>Une ligne html </p>      
+      <p id="ici">... paragraphe vide ...</p>
+      </p>Une autre ligne html</p>
+      <script>
+      document.getElementById("ici").innerHTML = "Bonjour de <em>Javascript</em>";
+      </script> 
+   </body>
+</html>
